7d6b39da6650993ef6e0344bbe6828fde28b71be
Peter van Ooy <pevanooy@gmail.com>
2017-09-22 17:27:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addToCart(item) {
  var price = Math.floor(Math.random() * 100);
  var myItem = {[item]: price};
  cart.push(myItem);
- console.log("has been added to your cart.")
+ console.log(`${item} has been added to your cart.`)
  return cart;
 }
 
