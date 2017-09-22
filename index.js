84da94bc1428982c91054736571c5872c2f74057
Peter van Ooy <pevanooy@gmail.com>
2017-09-22 17:40:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if (cart.length == 0){
-      console.log("Your shopping cart is empty.";);
+      console.log("Your shopping cart is empty.");
   }
 }
 
