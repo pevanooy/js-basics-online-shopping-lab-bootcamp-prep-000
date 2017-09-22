97339d138e734516cdfeb5bb6d9907ca1e07862e
Peter van Ooy <pevanooy@gmail.com>
2017-09-22 18:03:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function viewCart() {
   else if (cart.length == 1){
     var myObj = cart[0];
     var retString =  `In your cart, you have ${myObj.key} at \$${myObj[myObj.key]}.`
+    debugger
     console.log(retString)
     return retString
   }
