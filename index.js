49203b567ee740f2f9b77be70604bb47aedda544
Peter van Ooy <pevanooy@gmail.com>
2017-09-22 20:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,4 +77,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber.length > 0){
+  }
+  else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
